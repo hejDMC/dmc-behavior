20dd35aaebfd8e3bd26bfd8ae45f71f7031b9a64
2p laptop <jung.neurosc@gmail.com>
2025-01-13 14:30:06 +0100

    debug new task structure

--- code/test_read.py
+++ code/test_read.py
@@ -7,7 +7,7 @@ pin = 11
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(pin,GPIO.IN)
-time.sleep(0.25)
+time.sleep(0.5)
 while True:
    print(GPIO.input(pin))
 
