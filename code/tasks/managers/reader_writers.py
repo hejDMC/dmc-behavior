3c0fa60c36390d90c642a55785945c7d0a61d28d
felix-office <jung.neurosc@gmail.com>
2025-01-17 16:26:34 +0100

    sync test

--- code/tasks/managers/reader_writers.py
+++ code/tasks/managers/reader_writers.py
@@ -116,58 +116,61 @@ class RotaryRecorder(BaseRecorder):
 #         self.write_data(sync_value)
 #         time.sleep(1 / self.rate)
 
-
-class SyncRecorder(BaseRecorder):
-    def __init__(self, path_manager, exp_dir, task_type):
-        super().__init__(
-            path_manager,
-            exp_dir,
-            task_type,
-            file_name_suffix="sync_pulse_data",
-            rate_key="2p_sync_rate",
-        )
-        self.sync_pin = self.droid_settings["pin_map"]["IN"]["microscope_sync"]
-        self.sync_pulse = Sync_Pulse(self.sync_pin)
-        self.sync_pulse_list = []
-        self.old_value = 0
-
-    def record(self):
-        sync_value = str(self.sync_pulse.get_value())
-        if sync_value != self.old_value:
-            self.sync_pulse_list.append([time.time(), sync_value])
-            # self.write_data(sync_value)
-        self.old_value = sync_value
-        if self.stop:
-            self.writer.writerows(self.sync_pulse_list)
-
-# class SyncRecorder(threading.Thread):
+#
+# class SyncRecorder(BaseRecorder):
 #     def __init__(self, path_manager, exp_dir, task_type):
-#         super().__init__()
-#         data_io = DataIO(path_manager, task_type)
-#         self.droid_settings = data_io.load_droid_setting()
-#         self.fn = exp_dir.joinpath(f"{path_manager.get_today()}_sync_pulse_data.csv")
-#         self.running = False
-#         self.file = open(self.fn, mode='w', newline='')
-#         self.writer = csv.writer(self.file)
-#         self.writer.writerow(["Timestamp", "PinState"])  # CSV header
-#
-#         self.stop = False
-#
+#         super().__init__(
+#             path_manager,
+#             exp_dir,
+#             task_type,
+#             file_name_suffix="sync_pulse_data",
+#             rate_key="2p_sync_rate",
+#         )
 #         self.sync_pin = self.droid_settings["pin_map"]["IN"]["microscope_sync"]
-#         GPIO.setup(self.sync_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-#         # self.sync_pulse = Sync_Pulse(self.sync_pin, callback=self._transition_occurred)
+#         self.sync_pulse = Sync_Pulse(self.sync_pin)
+#         self.sync_pulse_list = []
+#         self.old_value = 0
 #
-#     def run(self):
-#         GPIO.add_event_detect(self.sync_pin, GPIO.RISING, callback=self._transition_occurred)
-#         while not self.stop:
-#             pass
-#         GPIO.remove_event_detect(self.sync_pin)  # Remove event detection
-#         self.file.close()
-#
-#     def _transition_occurred(self, pin):
-#         if not self.stop:
-#             self.writer.writerow([time.time(), 1])
-#             self.file.flush()  # Ensure data is written immediately
+#     def record(self):
+#         sync_value = str(self.sync_pulse.get_value())
+#         if sync_value != self.old_value:
+#             self.sync_pulse_list.append([time.time(), sync_value])
+#             # self.write_data(sync_value)
+#         self.old_value = sync_value
+#         if self.stop:
+#             self.writer.writerows(self.sync_pulse_list)
+
+class SyncRecorder(threading.Thread):
+    def __init__(self, path_manager, exp_dir, task_type):
+        super().__init__()
+        data_io = DataIO(path_manager, task_type)
+        self.droid_settings = data_io.load_droid_setting()
+        self.fn = exp_dir.joinpath(f"{path_manager.get_today()}_sync_pulse_data.csv")
+        self.running = False
+        self.file = open(self.fn, mode='w', newline='')
+        self.writer = csv.writer(self.file)
+        self.writer.writerow(["Timestamp", "PinState"])  # CSV header
+        self.sync_pulse_list = []
+
+        self.stop = False
+
+        self.sync_pin = self.droid_settings["pin_map"]["IN"]["microscope_sync"]
+        GPIO.setup(self.sync_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        # self.sync_pulse = Sync_Pulse(self.sync_pin, callback=self._transition_occurred)
+
+    def run(self):
+        GPIO.add_event_detect(self.sync_pin, GPIO.RISING, callback=self._transition_occurred)
+        while not self.stop:
+            pass
+        self.writer.writerows(self.sync_pulse_list)
+        GPIO.remove_event_detect(self.sync_pin)  # Remove event detection
+        self.file.close()
+
+    def _transition_occurred(self, pin):
+        if not self.stop:
+            self.sync_pulse_list.append([time.time(), 1])
+            # self.writer.writerow([time.time(), 1])
+            # self.file.flush()  # Ensure data is written immediately
 
 
 # class SyncRecorder(threading.Thread):
